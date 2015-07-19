d6c6e71c83e9d51f4beee4feb9ee59db93a4c26a
Facundo Rubin <facundorubin@gmail.com>
2015-07-19 19:28:24 -0300

    Agregue validacion de nya

--- clases/Inscripto.js
+++ clases/Inscripto.js
@@ -1,34 +1,44 @@
-
 /**  
   Clase Inscripo.
 **/
-Inscripo = function (pDni,pNom,pApe) {
-    this.nombre=pNom;
-    this.apellido=pApe;
+Inscripo = function (pDni,pNom,pApe) 
+{
+    this.nombre=this.validar_nya(pNom);
+    this.apellido=this.validar_nya(pApe);
     this.dni=this.validar_dni(pDni);
     this.valido=false;
-  }
+}
   
-  Inscripo.prototype.validar_dni = function(pDni)
-  {
+Inscripo.prototype.validar_dni = function(pDni)
+{
     
     var tem = sin_puntos(pDni);
 
     tem=this.validar(/^[0-9]{8}$/,tem);
     
     return tem;
-  }  
+}  
 
-  Inscripo.prototype.validar = function(formato,campo)
-  {
-    if ((campo.match(formato)) && (campo.value!='')) {
+Inscripo.prototype.validar_nya = function(pDato)
+{
+    
+    var tem = minusculas(pDato);
+
+    tem=this.validar(/^[a-z]{3,10}$/,tem);
+    
+    return tem;
+}
+
+Inscripo.prototype.validar = function(formato,campo)
+{
+  if ((campo.match(formato)) && (campo.value!='')) {
         this.valido=true;
         return campo; 
-    } else {
+  } else {
         this.valido=false;     
         return false;
-    } 
-  }
+  } 
+}
 
 
 /**  
@@ -40,4 +50,9 @@ var sin_puntos = function(pDni)
     return pDni.replace(/[\.]/g,'');
   }
  
+var minusculas= function(pDato)
+  {
+    return pDato.toLowerCase();
+  } 
+
 module.exports = Inscripo;
--- test/test.js
+++ test/test.js
@@ -15,11 +15,29 @@ describe('Deberia validar el parametro Dni',function(){
   
   });
 
-  it('Deberia devolver fallar si el dni es menor a 8 char',
+  it('Deberia fallar si el dni es menor a 8 char',
   	function()
   	{
   		var juan = new Inscripto('12.345.678.9','Juan','Perez');
   		expect(juan.dni).to.equal(false);	
   		expect(juan.valido).to.equal(false);					
   	});
+});
+
+
+describe('Deberia validar nombre',function(){
+	it('Deberia devolver nombre en minusculas',function(){
+		var juan = new Inscripto('12.345.678','JuAn','Perez');
+  		expect(juan.nombre).to.equal('juan');	
+	});
+
+	it('Deberia devolver false porque el nombre es menor a 3 char',function(){
+		var juan = new Inscripto('12.345.678','Ju','Perez');
+  		expect(juan.nombre).to.equal(false);	
+	});
+
+	it('Deberia devolver false porque el nombre posee numeros',function(){
+		var juan = new Inscripto('12.345.678','Ju35f2','Perez');
+  		expect(juan.nombre).to.equal(false);	
+	});
 });
