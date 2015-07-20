904fc643ae31d56f2d478bd85b5d5aa71ff23f62
Facundo Rubin <facundorubin@gmail.com>
2015-07-20 18:51:01 -0300

    Agregue prueba de Db

--- test/test_db.js
+++ test/test_db.js
@@ -1,12 +1,14 @@
 var expect = require('chai').expect,
-    Db = require('../clases/Db');
+    Db = require('../clases/Db')
+    conf = require('../config');
 
-describe('Deberia insertar un nuevo inscripto',function() {
-	it('',function() {
-		var db = new Db();
-		db.conectar();
-		var datos = db.consulta('SHOW tables;');
-		db.cerrar();
-		console.log(datos);
+describe('Probando la clase Db',function() {
+	it('Deberia mostrar las Tables_in_teyet',function() {
+		var db = new Db(conf);
+		db.consulta('SHOW tables;',function(res)
+			{
+				var tables="[ { Tables_in_teyet: 'condiciones_iva' },{ Tables_in_teyet: 'datos_facturacion' },{ Tables_in_teyet: 'estados_inscripcion' },{ Tables_in_teyet: 'formas_pago' },{ Tables_in_teyet: 'inscripciones' },{ Tables_in_teyet: 'original' },{ Tables_in_teyet: 'personas' },{ Tables_in_teyet: 'tipos_inscripcion' } ]";
+				expect(res).to.equal(tables);
+			});
 	});
 })
